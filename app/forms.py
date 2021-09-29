f55e7c34077bb291bd9526334347aca714434ee8
Stevenbarnett1 <Stevenbarnett1@live.com>
2021-09-29 15:09:57 -0700

    finished login

--- app/forms.py
+++ app/forms.py
@@ -1,12 +1,12 @@
 from wsgiref.validate import validator
 from flask_wtf import FlaskForm
-from wtforms import StringField, SelectField, SubmitField
+from wtforms import StringField, SelectField, SubmitField, PasswordField
 from wtforms.validators import DataRequired
 
 # class NewBookForm(FlaskForm):
 #     title = StringField("Title", validators=[DataRequired()])
-#     rating = SelectField("Rating", choices=["New favorite", "Great", 
-#                                     "Good", "Underwhelming", 
+#     rating = SelectField("Rating", choices=["New favorite", "Great",
+#                                     "Good", "Underwhelming",
 #                                     "Bad", "Horrible"])
 #     submit = SubmitField("Add Book")
 
@@ -14,4 +14,3 @@ class LoginForm(FlaskForm):
     employee_number = StringField("Employee Number", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Login")
-    
